aff40cabc66c229b8aaa26425b79a3f53c1cbe1d
k <k@klabz.org>
2019-11-25 21:09:57 +0100

    Update libs

--- src/SubCateg.hx
+++ src/SubCateg.hx
@@ -1,15 +1,12 @@
-
 // it's just easier with this lib
 import classnames.ClassNames.fastNull as classNames;
+
 import MyTheme.CGColors;
+import css.Properties;
 import mui.core.styles.Classes;
 import mui.core.styles.Styles;
 import react.ReactComponent;
 import react.ReactMacro.jsx;
-import react.types.*;
-import react.types.css.JustifyContent;
-import react.types.css.AlignContent;
-import react.types.css.Properties;
 
 private typedef Props = {
 	> PublicProps,
@@ -17,10 +14,10 @@ private typedef Props = {
 }
 
 private typedef PublicProps = {
-    ?label:String,
-    ?onclick:Void->Void,
-    ?colorClass:String,
-    ?icon: String,
+	?label:String,
+	?onclick:Void->Void,
+	?colorClass:String,
+	?icon: String,
 }
 
 private typedef TClasses = Classes<[
@@ -31,55 +28,54 @@ private typedef TClasses = Classes<[
 @:publicProps(PublicProps)
 @:wrap(Styles.withStyles(styles))
 class SubCateg extends ReactComponentOfProps<Props> {
-    
-    public static function styles(theme:MyTheme):ClassesDef<TClasses> {
-		return {
-            labelShip : {
-                backgroundColor: CGColors.White,    
-                fontSize: "0.95rem",
-                margin: "10 8",
-                padding: "5 10",
-                borderRadius: 16,
-                textDecoration: "none",
-                display: "inline-block",
 
-                transition: "all 0.5s ease",
+	public static function styles(theme:MyTheme):ClassesDef<TClasses> {
+		return Styles.jss({
+			labelShip : {
+				backgroundColor: CGColors.White,
+				fontSize: "0.95rem",
+				margin: "10 8",
+				padding: "5 10",
+				borderRadius: 16,
+				textDecoration: "none",
+				display: "inline-block",
 
-                "&::hover" : {
-                    backgroundColor: "#FFFFFF",//untyped color('#FFFFF').darken(10).hex(),
-                },       
+				transition: "all 0.5s ease",
 
-                "&.cagSelect" : {
-                    color:'#E56403',
-                },
-                
-                "&.cagLabelRouge" : {
-                    color:'#E53909',
-                },
+				"&::hover" : {
+					backgroundColor: "#FFFFFF",//untyped color('#FFFFF').darken(10).hex(),
+				},
 
-                "&.cagBio" : {
-                    color:'#16993B',
-                },
-            }
-		}
+				"&.cagSelect" : {
+					color:'#E56403',
+				},
+
+				"&.cagLabelRouge" : {
+					color:'#E53909',
+				},
+
+				"&.cagBio" : {
+					color:'#16993B',
+				},
+			}
+		});
 	}
 
-    override function render() {
-        var linkClasses = classNames({
+	override function render() {
+		var linkClasses = classNames({
 			'${props.classes.labelShip}': true,
 			'${props.colorClass}': true,
 		});
-        
-        var iconClasses = classNames({
-            '${props.icon}' : true,
-        });
 
-        var others = react.ReactMacro.getExtraProps(props);
-        return jsx('
-            <a onClick=${props.onclick} className=${linkClasses} {...others}>
-                <i className=${iconClasses}></i> ${props.label}
-            </a>
-        ');
-    }
+		var iconClasses = classNames({
+			'${props.icon}' : true,
+		});
+
+		return jsx(
+			<a onClick={props.onclick} className={linkClasses}>
+				<i className={iconClasses}></i> {props.label}
+			</a>
+		);
+	}
 }
 
